5ceeebf8c233f4ccd8d2f5a247cff28743581d1f
Abir Mahmud <abir2mahmud@gmail.com>
2026-01-10 16:13:44 +0600

    complete the feature: Photos

--- src/windows/Photos.jsx
+++ src/windows/Photos.jsx
@@ -1,10 +1,9 @@
 import { Mail, Search } from "lucide-react";
 
-import windowWrapper from "#hoc/WindowWrapper.jsx";
-import WindowControls from "#components/WindowControls.jsx";
-import { gallery, photosLinks} from "#constants";
-import use
-import useWindowStore from "#store/window.js"; WindowStore from "#store/Window";
+import WindowControls from "#components/WindowControls";
+import { gallery, photosLinks } from "#constants";
+import useWindowStore from "#store/window";
+import WindowWrapper from "#hoc/WindowWrapper";
 
 const Photos = () => {
     const { openWindow } = useWindowStore();
@@ -13,9 +12,42 @@ const Photos = () => {
         <>
           <div id="window-header">
               <WindowControls target="photos" />
-
-
+              <div className= "w-full flex justify-end items-center gap-3 text-gray-500">
+                  <Mail className="icon" />
+                  <Search className="icon" />
+              </div>
           </div>
+
+              <div className="flex w-full">
+                <div className="sidebar">
+                    <h2>Photos</h2>
+
+                    <ul>
+                        {photosLinks.map(({ id, icon, title }) => (
+                            <li key={id}>
+                                <img src={icon} alt={title} />
+                                <p>{title}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="gallery">
+                    <ul>
+                        {gallery.map(({ id, img }) => (
+                            <li key={id} onClick={() => openWindow("imgfile", { id, name: "Gallery image", icon: "/images/image.png", kind: "file", fileType: "img", imageUrl: img })}>
+                                <img src={img} alt={`Gallery image ${id}`} />
+                            </li>
+                            )
+                        )}
+                    </ul>
+                </div>
+              </div>
         </>
-    )
-}
+    );
+};
+
+const PhotosWindow = WindowWrapper(Photos, "photos");
+PhotosWindow.displayName = "Photos";
+
+export default PhotosWindow;
